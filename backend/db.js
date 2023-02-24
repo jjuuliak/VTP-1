10a765310cca38089cd948cc973a5e5b5887e882
Vaino Liukko <vaino.liukko@tuni.fi>
2023-02-24 11:20:21 +0200

    Removed unnessesary build stage

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
+  password: process.env.DB_PASSWORD || 'secret',
   database: process.env.DB_NAME || 'mydatabase',
   multipleStatements: true
 });
