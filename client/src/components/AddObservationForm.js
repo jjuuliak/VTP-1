8ab3b5b04e2edf6a91367df5e4b720e7c4324d6a
jjuuliak <ketolajuulia@gmail.com>
2023-03-24 12:05:19 +0200

    #83 More things to adding observation

--- client/src/components/AddObservationForm.js
+++ client/src/components/AddObservationForm.js
@@ -17,7 +17,6 @@ import {
   TableRow,
   Paper,
   Typography,
-  Box,
 } from "@mui/material";
 import { useTranslation } from "react-i18next";
 
@@ -35,14 +34,38 @@ const AddObservationForm = ({ open, handleClose, handleSubmit }) => {
   });
 
   const [criteria, setCriteria] = useState([]);
+  const [newCriterion, setNewCriterion] = useState("");
+  const [showNewCriterionInput, setShowNewCriterionInput] = useState(false);
+  const [inspectionMethodsTasks, setInspectionMethodsTasks] = useState([]);
+  const [showNewInspectionMethodsTasksInput, setShowNewInspectionMethodsTasksInput] = useState(false);
 
-  const handleInputChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const handleCriterionInputChange = (e) => {
+    setNewCriterion(e.target.value);
   };
 
   const handleAddCriterion = () => {
-    setCriteria([...criteria, formData.criterion]);
-    setFormData({ ...formData, criterion: '' });
+    setCriteria([...criteria, newCriterion]);
+    setNewCriterion("");
+    setShowNewCriterionInput(false);
+  };
+
+  const toggleNewCriterionInput = () => {
+    setShowNewCriterionInput(!showNewCriterionInput);
+  };
+
+  const toggleNewInspectionMethodsTasksInput = () => {
+    setShowNewInspectionMethodsTasksInput(!showNewInspectionMethodsTasksInput);
+  };
+
+  const handleAddInspectionMethodsTasks = () => {
+    setInspectionMethodsTasks([...inspectionMethodsTasks, formData.inspectionMethods]);
+    setFormData({ ...formData, inspectionMethods: "" });
+    setShowNewInspectionMethodsTasksInput(false);
+  };  
+
+  const handleInputChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
   const submitForm = () => {
@@ -102,18 +125,43 @@ const AddObservationForm = ({ open, handleClose, handleSubmit }) => {
             <TableBody>
             {criteria.map((criterion, index) => (
                 <TableRow key={index}>
-                  <TableCell>{criterion.title}</TableCell>
-                  <TableCell>{criterion.content}</TableCell>
+                  <TableCell>{criterion}</TableCell>
                 </TableRow>
               ))}
             </TableBody>
           </Table>
         </TableContainer>
-        <Box mt={1}>
-          <Button variant="contained" onClick={handleAddCriterion}>
-            {t('addObservation.add')}
+        {showNewCriterionInput && (
+          <TextField
+            margin="dense"
+            id="newCriterionContent"
+            label={t("addObservation.newCriterionContent")}
+            type="text"
+            fullWidth
+            value={newCriterion}
+            onChange={handleCriterionInputChange}
+            style={{ marginTop: 16 }}
+          />
+        )}
+        {!showNewCriterionInput ? (
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={toggleNewCriterionInput}
+            style={{ marginTop: 16 }}
+          >
+            {t("addObservation.add")}
           </Button>
-        </Box>
+        ) : (
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={handleAddCriterion}
+            style={{ marginTop: 16, marginLeft: 16 }}
+          >
+            {t("addObservation.saveObservation")}
+          </Button>
+        )}
         <TextField
           margin="dense"
           id="observation"
@@ -163,17 +211,52 @@ const AddObservationForm = ({ open, handleClose, handleSubmit }) => {
             <MenuItem value="4">{t('addObservation.veryHighSignificance')}</MenuItem>
           </Select>
         </FormControl>
-        <TextField
-          margin="dense"
-          id="inspectionMethods"
-          name="inspectionMethods"
-          label={t('addObservation.inspectionMethodsTasks')}
-          type="text"
-          fullWidth
-          value={formData.inspectionMethods}
-          onChange={handleInputChange}
-          required
-        />
+        <Typography variant="h6" gutterBottom>
+          {t('addObservation.inspectionMethodsTasks')}
+        </Typography>
+        <TableContainer component={Paper}>
+          <Table>
+            <TableBody>
+              {inspectionMethodsTasks.map((method, index) => (
+                <TableRow key={index}>
+                  <TableCell>{method}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        {!showNewInspectionMethodsTasksInput ? (
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={toggleNewInspectionMethodsTasksInput}
+            style={{ marginTop: 16 }}
+          >
+            {t("addObservation.add")}
+          </Button>
+        ) : (
+          <>
+            <TextField
+              margin="dense"
+              id="newInspectionMethodsTasks"
+              name="inspectionMethods"
+              label={t("addObservation.newCriterionContent")}
+              type="text"
+              fullWidth
+              value={formData.inspectionMethods}
+              onChange={handleInputChange}
+              required
+            />
+            <Button
+              variant="contained"
+              color="secondary"
+              onClick={handleAddInspectionMethodsTasks}
+              style={{ marginTop: 16, marginLeft: 16 }}
+            >
+              {t("addObservation.saveObservation")}
+            </Button>
+          </>
+        )}
       </DialogContent>
       <DialogActions>
         <Button onClick={handleClose}>{t('addObservation.cancel')}</Button>
