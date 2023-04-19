c09d0475780336c02766f2c82b76d7c3960a648a
Anna Knappe <104974789+anna-knappe@users.noreply.github.com>
2023-04-19 15:25:18 +0300

    DataContainer style fix

--- client/src/components/DataContainer.js
+++ client/src/components/DataContainer.js
@@ -4,6 +4,8 @@ import InspectionInformation from './InspectionInformation';
 import LatestDocuments from './LatestDocuments';
 import Scheduling from './Scheduling';
 import TargetTimeframe from './TargetTimeframe';
+import Card from './ui/Card';
+import PageContainer from './ui/PageContainer';
 
 const DataContainer = () => {
   const { inspectionId } = useParams();
@@ -20,15 +22,23 @@ const DataContainer = () => {
   };
 
   return (
-    <>
-      <TargetTimeframe targetTimeframeData={draftData.target_timeframes} />
-      <InspectionInformation inspectionData={draftData} />
-      <LatestDocuments latestDocuments={draftData.documents} />
-      <Scheduling 
-        events={draftData.scheduling}
-        setEvents={handleSchedulingDataUpdate}
-        draftId={draftData.draft_id} />
-    </>
+    <PageContainer>
+      <Card>
+        <TargetTimeframe targetTimeframeData={draftData.target_timeframes} />
+      </Card>
+      <Card>
+        <InspectionInformation inspectionData={draftData} />
+      </Card>
+      <Card>
+        <LatestDocuments latestDocuments={draftData.documents} />
+      </Card>
+      <Card>
+        <Scheduling
+          events={draftData.scheduling}
+          setEvents={handleSchedulingDataUpdate}
+          draftId={draftData.draft_id} />
+      </Card>
+    </PageContainer>
   );
 };
 
